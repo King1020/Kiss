691f3a4176dd7f60aa68a48c7314850af5f61e02
liqi <12345li@qq.com>
2019-08-12 15:11:34 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {HashRouter,BrowserRouter,Route,Switch} from 'react-router-dom'
+import Admin from './pages/admin/admin.jsx'
+import Login from './pages/login/login.jsx'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ export default class App extends Component {
+    render() {
+        return (
+         <BrowserRouter>
+             <Switch>
+                 <Route path="/admin" component={Admin}/>
+                 <Route path="/login" component={Login}/>
+             </Switch>
+         </BrowserRouter>
+       
+        )
+    }
 }
-
-export default App;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+// 登录路由
+export default class Loagin extends Component {
+    render() {
+        return (
+            <div>
+               Loagin
+            </div>
+        )
+    }
+}
